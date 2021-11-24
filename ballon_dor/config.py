ae76c1432e1316dacd8d9dbaca83452ec5750b56
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-11-24 16:36:09 +0100

    added players positions csv config

--- ballon_dor/config.py
+++ ballon_dor/config.py
@@ -3,3 +3,4 @@ from pathlib import Path
 
 
 CSV_PATH = Path(os.environ.get('CSV_BALLON_DOR_PATH', 'BallonDOr.csv'))
+POSITIONS_CSV_PATH = Path(os.environ.get('CSV_POSITIONS_PATH', 'players_positions.csv'))
