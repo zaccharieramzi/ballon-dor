7d3cfcd064ead7e820a3f19ac01943a21a661158
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-11-24 16:37:23 +0100

    allowed adding players positions to general df

--- ballon_dor/new_rankings.py
+++ ballon_dor/new_rankings.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
-from ballon_dor.config import CSV_PATH
+from ballon_dor.config import CSV_PATH, CSV_POSITIONS_PATH
+from ballon_dor.get_positions import save_players_positions
 
 
 DF_BALLON_DOR = pd.read_csv(CSV_PATH)
+RELOAD_POSITIONS = False
 
 
 def get_ranking_on_period(year_begin, year_end):
@@ -16,6 +18,13 @@ def get_ranking_on_period(year_begin, year_end):
     return new_ranking
 
 
-r = get_ranking_on_period(2000, 2018)
-for i, (player, share) in enumerate(r.iteritems(), start=1):
-    print(f'{i:3d}: {player:<50s} {share:.5f}')
+# r = get_ranking_on_period(2000, 2018)
+# for i, (player, share) in enumerate(r.iteritems(), start=1):
+#     print(f'{i:3d}: {player:<50s} {share:.5f}')
+
+if RELOAD_POSITIONS:
+    save_players_positions(DF_BALLON_DOR)
+else:
+    df_positions = pd.read_csv(CSV_POSITIONS_PATH)
+    df = pd.merge(DF_BALLON_DOR, df_positions, on='Player')
+    
